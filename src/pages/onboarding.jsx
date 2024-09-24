42edb5df7c9eb8244f759a73fc6f92efd66f14c5
Aditya <adityaaryan11k@gmail.com>
2024-09-24 17:11:05 +0530

    update-1.0

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -1,35 +1,41 @@
-import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { BarLoader } from "react-spinners";
 
-const OnboardingPage = () => {
+const Onboarding = () => {
   const { user, isLoaded } = useUser();
   const navigate = useNavigate();
+
+  const navigateUser = (currRole) => {
+    navigate(currRole === "recruiter" ? "/post-job" : "/jobs");
+  };
+
   const handleRoleSelection = async (role) => {
     await user
       .update({ unsafeMetadata: { role } })
       .then(() => {
-        navigate(role === "recruiter" ? "/post-job" : "/jobs");
+        console.log(`Role updated to: ${role}`);
+        navigateUser(role);
       })
       .catch((err) => {
-        console.log("Error Updating Role:", err);
+        console.error("Error updating role:", err);
       });
   };
 
-  useEffect(()=>{
-    if(user?.unsafeMetadata.role){
-      navigate(user.unsafeMetadata.role === "recruiter" ? "/post-job" : "/jobs");
+  useEffect(() => {
+    if (user?.unsafeMetadata?.role) {
+      navigateUser(user.unsafeMetadata.role);
     }
-  },[user])
+  }, [user]);
 
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
 
   return (
-    <div className="flex flex-col items-center justify-center mt-32">
+    <div className="flex flex-col items-center justify-center mt-40">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
         I am a...
       </h2>
@@ -42,7 +48,7 @@ const OnboardingPage = () => {
           Candidate
         </Button>
         <Button
-          variant="red"
+          variant="destructive"
           className="h-36 text-2xl"
           onClick={() => handleRoleSelection("recruiter")}
         >
@@ -53,4 +59,4 @@ const OnboardingPage = () => {
   );
 };
 
-export default OnboardingPage;
+export default Onboarding;
